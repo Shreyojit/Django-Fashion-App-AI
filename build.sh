e7f2e8520d301d3bd5dffc9fda3016feee4942b8
Shreyojit <shreyojitdas95@gmail.com>
2025-01-03 21:43:40 +0530

    Entire application completeed added allowed host

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ pip install -r requirements.txt
 # Run migrations
 python manage.py migrate
 
-# Collect static files (if needed)
+# Collect static files (for production)
 python manage.py collectstatic --noinput
 
 # Start the Django app and bind it to the correct port
