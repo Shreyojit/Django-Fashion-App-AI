c6da824d14e451e1f21adb6694485994d8ea649b
Shreyojit <85859037+Shreyojit@users.noreply.github.com>
2025-01-04 00:18:27 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,8 +11,15 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --no-input
 
+# Make migrations
 python manage.py makemigrations
 
 # Run migrations
 python manage.py migrate
+
+# Create superuser if needed
+if [[ ! $(python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username='shreyo').exists())") == "True" ]]; then
+  python manage.py createsuperuser --no-input --username "shreyo" --email "shreyo@example.com" --password "fashion2025"
+fi
+
 
