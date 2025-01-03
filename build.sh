3a3aadbee07037908f7ba224f19cb5dba3679f68
Shreyojit <shreyojitdas95@gmail.com>
2025-01-03 22:32:33 +0530

    changes

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
+#!/bin/bash
 
+# Create static directory if it doesn't exist
+mkdir -p static
+
+# Install dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Run migrations
 python manage.py migrate
